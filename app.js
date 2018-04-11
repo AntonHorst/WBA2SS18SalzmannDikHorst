b8d953e707d8769d4456c24edadb9881eb98bd18
Anton Horst <antonhorst92@gmail.com>
2018-04-11 16:02:48 +0200

    Aufgabe 2

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var fs = require('fs')
+const chalk = require('chalk');
 fs.readFile(__dirname+"/staedte.json", 'utf8', (err, data) => {
   if (err) {
     console.log("Fehler beim Laden der Dateien")
   } else {
   var jsonobj = JSON.parse(data);
-  //console.log(jsonobj.cities[0].population);
-
 }
 for(var i=0;i < data.length; i++){
-console.log("\n name:"+jsonobj.cities[i].name+"\n","country:"+jsonobj.cities[i].country+"\n","population:"+jsonobj.cities[i].population+"\n");
+console.log("\n name:"+chalk.blue(jsonobj.cities[i].name)+"\n",
+"country:"+chalk.red(jsonobj.cities[i].country)+"\n",
+"population:"+chalk.green(jsonobj.cities[i].population)+"\n");
 }
+
 });
