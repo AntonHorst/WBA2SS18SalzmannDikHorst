02c139dbbd211c373004d733d8e7de5616eb99c6
Anton Horst <antonhorst92@gmail.com>
2018-04-12 20:36:30 +0200

    Aufgabe 3

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var fs = require('fs')
 const chalk = require('chalk');
-fs.readFile(__dirname+"/staedte.json", 'utf8', (err, data) => {
-  if (err) {
-    console.log("Fehler beim Laden der Dateien")
-  } else {
-  var jsonobj = JSON.parse(data);
-}
-for(var i=0;i < data.length; i++){
-console.log("\n name:"+chalk.blue(jsonobj.cities[i].name)+"\n",
-"country:"+chalk.red(jsonobj.cities[i].country)+"\n",
-"population:"+chalk.green(jsonobj.cities[i].population)+"\n");
+function compareNumbers(a,b){
+  return a.population-b.population;
 }
 
-});
+
+
+fs.readFile(__dirname+'/staedte.json','utf8',(err,data) => {
+  var jsonobj = JSON.parse(data);
+
+  jsonobj.cities.sort(compareNumbers);
+  var string = JSON.stringify(jsonobj, null, 2);
+
+
+  fs.writeFile(__dirname+'/staedte_sortiert.json',string,function(err) {
+    if(err) return console.error(err);
+
+    for(var i=0;i < data.length; i++){
+    console.log("\n name:"+chalk.blue(jsonobj.cities[i].name)+"\n",
+    "country:"+chalk.red(jsonobj.cities[i].country)+"\n",
+    "population:"+chalk.green(jsonobj.cities[i].population)+"\n");
+    }
+  })
+})
