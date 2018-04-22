32e19a36a709871d9fd591cadf2fda07b4540b4b
Anton Horst <antonhorst92@gmail.com>
2018-04-22 21:16:47 +0200

    Aufgabe 2-2 funktioniert aber ist noch nicht formatiert

--- Aufgaben/Aufgabe2/app.js
+++ Aufgaben/Aufgabe2/app.js
@@ -1,8 +1,45 @@
 var fs = require('fs');
-var concat =require('array-concat');
-fs.readFile(__dirname+'/staedte.json','utf8',(err,data1) => {
-  fs.readFile(__dirname+'/mehr_staedte.json','utf8',(err,data2) => {
-      var zusammen = data1.concat(data2);
-      console.log(JSON.parse(zusammen));
-  })
+var filename = __dirname+'/staedte.json';
+
+
+
+var promise1 = new Promise(function(resolve, reject) {
+  fs.readFile(__dirname+'/staedte.json','utf8',(err,data) => {
+    var jsonobj1 = JSON.parse(data);
+    if(err){
+      reject(console.log('asduabsdad'));
+    }else{
+      resolve(jsonobj1.cities);
+    }
+  }
+)})
+
+var promise2 = new Promise(function(resolve, reject) {
+  fs.readFile(__dirname+'/mehr_staedte.json','utf8',(err,data) => {
+    var jsonobj2 = JSON.parse(data);
+    if(err){
+      reject(console.log('asduabsdad'));
+    }else{
+      resolve(jsonobj2.cities);
+    }
+  }
+)})
+
+var ausgabe = Promise.all([promise1, promise2]).then(function(fullfilled) {
+	console.log(fullfilled);
 })
+.catch(function(error) {
+	// One or more promises was rejected
+});
+
+/*var ausgabe = function(){
+  promise1
+  .then(function (fullfilled){
+    console.log(fullfilled);
+  })
+  .catch(function (error){
+
+    console.log('basdbuazsbd');
+  });
+};
+ausgabe();*/
