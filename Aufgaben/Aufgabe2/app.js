372e0f1c653f339c6d32c1343330d074a642d470
Anton Horst <antonhorst92@gmail.com>
2018-05-07 11:50:28 +0200

    Aufgabe 3 fertig

--- Aufgaben/Aufgabe2/app.js
+++ Aufgaben/Aufgabe2/app.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var filename = __dirname+'/staedte.json';
+const chalk = require('chalk');
 
 
 
@@ -7,7 +8,7 @@ var promise1 = new Promise(function(resolve, reject) {
   fs.readFile(__dirname+'/staedte.json','utf8',(err,data) => {
     var jsonobj1 = JSON.parse(data);
     if(err){
-      reject(console.log('asduabsdad'));
+      reject(console.log('Ein Fehler ist beim Lesen der staedte.json aufgetreten.'));
     }else{
       resolve(jsonobj1.cities);
     }
@@ -18,7 +19,7 @@ var promise2 = new Promise(function(resolve, reject) {
   fs.readFile(__dirname+'/mehr_staedte.json','utf8',(err,data) => {
     var jsonobj2 = JSON.parse(data);
     if(err){
-      reject(console.log('asduabsdad'));
+      reject(console.log('Ein Fehler ist beim Lesen der staedte_mehr.json aufgetreten.'));
     }else{
       resolve(jsonobj2.cities);
     }
@@ -26,7 +27,16 @@ var promise2 = new Promise(function(resolve, reject) {
 )})
 
 Promise.all([promise1, promise2]).then(function(fullfilled) {
-	console.log(fullfilled);
+//  fullfilled= JSON.parse(fullfilled);
+	//console.log(fullfilled[0][2]);
+  for(var j =0; j<fullfilled.length; j++){
+    for(var i=0; i<fullfilled[j].length; i++){
+    //console.log(fullfilled[j][i]);
+    console.log("\n name:"+chalk.blue(fullfilled[j][i].name)+"\n",
+    "country:"+chalk.red(fullfilled[j][i].country)+"\n",
+    "population:"+chalk.green(fullfilled[j][i].population)+"\n",
+    "\n--------------------");
+  }}
 })
 .catch(function(error) {
 });
@@ -38,7 +48,7 @@ Promise.all([promise1, promise2]).then(function(fullfilled) {
   })
   .catch(function (error){
 
-    console.log('basdbuazsbd');
+    console.log('test');
   });
 };
 ausgabe();*/
