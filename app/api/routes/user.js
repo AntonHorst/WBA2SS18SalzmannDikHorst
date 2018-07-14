11f439f5e1a953b7387dcb3ddea9c2536d8b63d0
Sven Salzmann <kleider.yoe@gmail.com>
2018-07-14 12:15:27 +0200

    getByUri hinzugefuegt

--- app/api/routes/user.js
+++ app/api/routes/user.js
@@ -21,7 +21,7 @@ router.get('/', (req, res, next) => {
                     artist_uri: doc.artist_uri,
                     request: {
                         type: 'GET',
-                        url: 'http://localhost:3000/user/' + doc._id
+                        url: 'http://localhost:3000/user/' + doc.uri
                     }
                 }
             })
@@ -46,43 +46,30 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:uri', (req, res, next) => {
-    User.find()
-    .select('name uri _id artists')
+    const uri = req.params.uri;
+    User.find({uri: uri })
+    .select('uri name _id artist_name artist_uri')
     .exec()
-    .then(docs => {
-        const response = {
-            count: docs.length,
-            tastes: docs.map(doc => {
-                return {
-                    name: doc.name,
-                    uri: doc.uri,
-                    _id: doc._id,
-                    artist_name: doc.artist_name,
-                    artist_uri: doc.artist_uri,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:3000/user/' + doc.uri
-                    }
+    .then(doc => {
+        console.log(doc);
+        if (doc) {
+            res.status(200).json({
+                user: doc,
+                request: {
+                    type: 'GET',
+                    description: 'GET_ALL_TASTES',
+                    url: 'http://localhost:3000/user/'
                 }
-            })
-        };
-        if (docs.length >= 0) {
-        res.status(200).json(response);
-        } else {
-            //not really 404, just no objects in database
-            res.status(404).json({
-                message: 'No entries found'
             });
+        } else {
+            res.status(404).json({message: 'No valid entry found with this ID.'});
         }
+        
     })
     .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    })
-
- 
-    
+        console.log(err)
+        res.status(500).json({error: err});
+    });
 });
 
 router.delete('/:userURI', (req, res, next) =>{
--- app/app.js
+++ app/app.js
@@ -249,23 +249,4 @@ app.get('/callback', function(req, res) {
     });
   });
 
- 
-
-
-/*
-app.use((req, res, next) =>{
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-});
-
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});*/
-
 module.exports = app;
