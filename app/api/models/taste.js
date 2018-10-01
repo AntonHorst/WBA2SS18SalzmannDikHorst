c0f16839bd5d68cb0b0679361b6e0f9c3cda5b67
Julian <jd@julians-imac.lan>
2018-10-01 10:31:00 +0200

    auslagerung

--- app/api/models/taste.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const tasteSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String},
-    popularity: { type: Number}
-});
-
-module.exports = mongoose.model('Taste', tasteSchema);
--- app/api/models/userList.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userListSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    uri: { type: String},
-});
-
-module.exports = mongoose.model('UserList', userListSchema);
